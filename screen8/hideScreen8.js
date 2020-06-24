63e17c08b0a85bdfd675c288adca17a22f43f74c
blokstruggles <58359738+blokstruggles@users.noreply.github.com>
2020-06-24 11:36:56 +0200

    Add files via upload

--- screen8/hideScreen8.js
+++ screen8/hideScreen8.js
@@ -33,7 +33,7 @@ function hideScreen8() {
   screen5Antwoord.hide();
   screen6Antwoord.hide();
   screen7Antwoord.hide();
-  
+
   screen3AntwoordAndere.hide();
   screen4AntwoordAndere.hide();
   screen5AntwoordAndere.hide();
@@ -44,35 +44,35 @@ function hideScreen8() {
   screen3meer.hide();
   screen3veelminder.hide();
   screen3minder.hide();
-  
+
   screen4veelmeer.hide();
   screen4meer.hide();
   screen4veelminder.hide();
   screen4minder.hide();
-  
+
   screen5veelmeer.hide();
   screen5meer.hide();
   screen5veelminder.hide();
   screen5minder.hide();
-  
+
   screen6veelmeer.hide();
   screen6meer.hide();
   screen6veelminder.hide();
   screen6minder.hide();
-  
+
   screen7veelmeer.hide();
   screen7meer.hide();
   screen7veelminder.hide();
   screen7minder.hide();
-  
+
   screen3hetzelfde.hide();
   screen4hetzelfde.hide();
   screen5hetzelfde.hide();
   screen6hetzelfde.hide();
   screen7hetzelfde.hide();
 
-  
-    logoRuimte.hide();
+
+  logoRuimte.hide();
   logoPrivacy.hide();
   logoUitzicht.hide();
   logoLicht.hide();
@@ -81,11 +81,11 @@ function hideScreen8() {
   emailVraag.hide();
   email.hide();
   emailTekst.hide();
-  
-  if(screen==10){
-  logo.show();
-  IC.show();
-  IC.value("Bedankt voor je deelname! Nog veel succes met je laatste examens!")
+
+  if (screen == 10) {
+    logo.show();
+    IC.show();
+    IC.value("Bedankt voor je deelname! Nog veel succes met je laatste examens!")
   }
 }
 
@@ -103,29 +103,52 @@ function showScreen8() {
 
   screen7Positief.show();
 
+  if (screen3VY <= camY + camH) {
   screen3V.show();
+    }
+  if (screen3XY <= camY + camH) {
   screen3X.show();
+  }
 
   screen3Antwoord.show();
   screen4Antwoord.show();
   screen5Antwoord.show();
   screen6Antwoord.show();
   screen7Antwoord.show();
-  
+
   screen3AntwoordAndere.show();
   screen4AntwoordAndere.show();
   screen5AntwoordAndere.show();
   screen6AntwoordAndere.show();
   screen7AntwoordAndere.show();
 
-  screen4V.show();
+  if (screen4VY <= camY + camH) {
+    screen4V.show();
+  }
+  if (screen4XY <= camY + camH) {
   screen4X.show();
+  }
+  
+  if (screen5VY <= camY + camH) {
   screen5V.show();
+    }
+  if (screen5XY <= camY + camH) {
   screen5X.show();
+  }
+  
+  if (screen6VY <= camY + camH) {
   screen6V.show();
+    }
+  if (screen6XY <= camY + camH) {
   screen6X.show();
+    }
+  
+  if (screen7VY <= camY + camH) {
   screen7V.show();
+    }
+  if (screen7XY <= camY + camH) {
   screen7X.show();
+  }
 
   if (antwoord3 == "veel meer") {
     screen3veelmeer.show();
@@ -191,27 +214,27 @@ function showScreen8() {
   if (antwoord7 == "minder") {
     screen7minder.show();
   }
-  
-  if (antwoord3=="hetzelfde"){
-  screen3hetzelfde.show();
+
+  if (antwoord3 == "hetzelfde") {
+    screen3hetzelfde.show();
   }
 
-    if (antwoord4=="hetzelfde"){
-  screen4hetzelfde.show();
+  if (antwoord4 == "hetzelfde") {
+    screen4hetzelfde.show();
   }
-  
-    if (antwoord5=="hetzelfde"){
-  screen5hetzelfde.show();
+
+  if (antwoord5 == "hetzelfde") {
+    screen5hetzelfde.show();
   }
-  
-    if (antwoord6=="hetzelfde"){
-  screen6hetzelfde.show();
+
+  if (antwoord6 == "hetzelfde") {
+    screen6hetzelfde.show();
   }
-  
-    if (antwoord7=="hetzelfde"){
-  screen7hetzelfde.show();
+
+  if (antwoord7 == "hetzelfde") {
+    screen7hetzelfde.show();
   }
-  
+
   logoRuimte.show();
   logoPrivacy.show();
   logoUitzicht.show();
